5ea57f4ffd7a73ca212894b0e6540e8b67ce391e
MVasiljev <milan.vasiljev.work@gmail.com>
2024-12-31 12:38:39 +0100

    wip

--- server/src/config/logger.ts
+++ server/src/config/logger.ts
@@ -20,8 +20,8 @@ const logger = createLogger({
         })
       ),
     }),
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" }),
+    // new transports.File({ filename: "logs/error.log", level: "error" }),
+    // new transports.File({ filename: "logs/combined.log" }),
   ],
 });
 
